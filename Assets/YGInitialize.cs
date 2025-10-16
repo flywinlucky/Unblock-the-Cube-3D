21a164664cfcee718ac133d404cdaa7977151536
Traffic Wolf <flywinlucky777@gmail.com>
2025-10-16 13:09:34 +0300

    rf

---
+++ Assets/YGInitialize.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class YGInitialize : MonoBehaviour
+{
+    private string currendLang;
+    private void Start()
+    {
+        YG2.GameReadyAPI();
+        Debug.Log("GameReady called — game is now ready for interaction.");
+
+        currendLang = YG2.envir.language;
+
+        ChangeLanguage(currendLang);
+    }
+
+    public void ChangeLanguage(string prefixLanguage)
+    {
+        Debug.Log("YGInitialize : Language changed to " + prefixLanguage);
+        YG2.SwitchLanguage(prefixLanguage);
+    }
+
+
+}
