336c8d552150045e3f2be01d365fe3e268248379
Traffic Wolf <flywinlucky777@gmail.com>
2025-10-23 22:32:07 +0300

    r

---
+++ Assets/Fly Studios Games/Tap Away Block Puzzle 3D/Scripts/YG/RewardAdCoins.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class RewardAdCoins : MonoBehaviour
+{  
+    public void ShowRewarded_GetCoins()
+    {
+        Debug.Log("Try Show Reward");
+
+        // Afișează reclama și dă recompensa dacă a fost vizionată complet
+        YG2.RewardedAdvShow("showrewardadd_coins", () =>
+        {
+            Debug.Log("Get Reward");
+        });
+    }
+}
