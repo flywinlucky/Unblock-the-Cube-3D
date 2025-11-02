d31f1fdce33c1cb50c6733738f358dc94624178a
Traffic Wolf <flywinlucky777@gmail.com>
2025-11-02 23:39:57 +0200

    rr

--- Assets/Fly Studios Games/Try To Count The Boxes Brain Training/Scripts/LevelManager.cs
+++ Assets/Fly Studios Games/Try To Count The Boxes Brain Training/Scripts/LevelManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-   
+    [Header("Player UI 1")]
+    public PlayerUI player_1_UI;
+    [Header("Player UI 2")]
+    public PlayerUI player_2_UI;
 }
---
+++ Assets/Fly Studios Games/Try To Count The Boxes Brain Training/Scripts/PlayerUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerUI : MonoBehaviour
+{
+    [Header("Player UI")]
+    public GameObject player_UI_panel;
+    public Text player_countCell_1_Text;
+    public Text player_countCell_2_Text;
+    public Text player_countButton_Keyboard_Key_Text;
+    public Text player_countButton_message_Text;
+       public string player_countButton_message_string;
+    public Text player_doneButton_Keyboard_Key_Text;
+    
+    public Text player_doneButton_message_Text;
+    public string player_doneButton_message_string;
+    
+    [Space]
+    public GameObject player_finalCountResult;
+    public Text player_finalCount_Text;
+    public Image player_resultIcon_image;
+    public Sprite player_trueFlag_icon_sprite;
+    public Sprite player_falseFlag_icon_sprite;
+}
