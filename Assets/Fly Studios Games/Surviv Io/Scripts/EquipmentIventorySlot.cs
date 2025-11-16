bca6d71c6f32d5cbe0aef9af6a125e203c023854
Traffic Wolf <flywinlucky777@gmail.com>
2025-11-16 20:34:35 +0200

    wef

---
+++ Assets/Fly Studios Games/Surviv Io/Scripts/EquipmentIventorySlot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipmentIventorySlot : MonoBehaviour
+{
+    public Image equipment_Icon_Image;
+    public Text equipment_level_Text;
+    public GameObject equipament_Gameobject;
+    public void RefreshEquipamentSlot(Sprite equipament_icon, int equipament_Level)
+    {
+        equipment_Icon_Image.sprite = equipament_icon;
+        equipment_level_Text.text = "LVL." + equipament_Level.ToString();
+    }
+}
