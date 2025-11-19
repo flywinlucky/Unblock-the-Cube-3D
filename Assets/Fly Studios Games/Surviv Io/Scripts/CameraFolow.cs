b65846682f4d40bb31b9ea196df12005c5730ad8
Traffic Wolf <flywinlucky777@gmail.com>
2025-11-19 16:52:16 +0200

    wef

--- Assets/Fly Studios Games/Surviv Io/Scripts/CameraFolow.cs
+++ Assets/Fly Studios Games/Surviv Io/Scripts/CameraFolow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraFolow : MonoBehaviour
 {
+    public BoxCollider2D arenaBounds;
     public Transform playerPosition;
 
     [SerializeField]
@@ -11,10 +12,13 @@ public class CameraFolow : MonoBehaviour
 
     private Vector3 _velocity = Vector3.zero;
     private float _cameraZPosition;
+    private Camera _cam;
 
     void Start()
     {
         _cameraZPosition = transform.position.z;
+        _cam = GetComponent<Camera>();
+        if (_cam == null) _cam = Camera.main;
     }
 
     void LateUpdate()
@@ -29,6 +33,30 @@ public class CameraFolow : MonoBehaviour
             playerPosition.position.y,
             _cameraZPosition
         );
+
+        // Clamp inside arena bounds (orthographic only)
+        if (arenaBounds != null && _cam != null && _cam.orthographic)
+        {
+            Bounds b = arenaBounds.bounds;
+            float halfH = _cam.orthographicSize;
+            float halfW = halfH * _cam.aspect;
+
+            // If bounds smaller than view, lock to center
+            float minX = b.min.x + halfW;
+            float maxX = b.max.x - halfW;
+            float minY = b.min.y + halfH;
+            float maxY = b.max.y - halfH;
+
+            if (b.size.x < halfW * 2f)
+                desiredPosition.x = b.center.x;
+            else
+                desiredPosition.x = Mathf.Clamp(desiredPosition.x, minX, maxX);
+
+            if (b.size.y < halfH * 2f)
+                desiredPosition.y = b.center.y;
+            else
+                desiredPosition.y = Mathf.Clamp(desiredPosition.y, minY, maxY);
+        }
 
         transform.position = Vector3.SmoothDamp(
             transform.position,
